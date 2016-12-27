927d6b8424d0b42206d14aa44eff107d8ebb0584
Ted Lilley <ted.lilley@accelerated.com>
2016-12-27 16:47:54 -0500

    start changing search_root

--- dctk/shpec/dctk_shpec.bash
+++ dctk/shpec/dctk_shpec.bash
@@ -103,12 +103,215 @@ describe 'search_root'
     dir=$($mktempd) || return 1
     touch "$dir"/file
     chmod +x "$dir"/file
-    result=$(search_root file dir)
-    assert equal "$dir"/file "$result"
+    result=$(search_root dir file)
+    printf -v expected 'declare -a result=%s([0]="%s/file")%s' \' "$dir" \'
+    assert equal "$expected" "$result"
     # shellcheck disable=SC2154
     cleanup "$dir"
     return "$_shpec_failures" )
   end
+
+#   it 'finds a command in a subdir'; (
+#     # shellcheck disable=SC2154
+#     dir=$($mktempd) || return 1
+#     $mkdir "$dir"/dir
+#     touch "$dir"/dir/file
+#     chmod +x "$dir"/dir/file
+#     result=$(search_root file dir)
+#     assert equal "$dir"/dir/file "$result"
+#     # shellcheck disable=SC2154
+#     cleanup "$dir"
+#     return "$_shpec_failures" )
+#   end
+#
+#   it 'finds a command in the root directory'; (
+#     dir=$($mktempd) || return 1
+#     touch "$dir"/mycommand
+#     chmod +x "$dir"/mycommand
+#     result=$(search_root dir mycommand)
+#     assert equal $? 0
+#     eval "$(assign resulta result)"
+#     # shellcheck disable=SC2154
+#     assert equal "$dir"/mycommand "${resulta[*]}"
+#     cleanup "$dir"
+#     return "$_shpec_failures" )
+#   end
+#
+#   it "doesn't find a command which doesn't exist"; ( _shpec_failures=0   # shellcheck disable=SC2030
+#     temp=$(make_temp_dir)
+#     validate_dirname "$temp" || return
+#     find_recursive "$temp" mycommand
+#     assert equal $? 1
+#     assert equal "${command[*]}" mycommand
+#     cleanup "$temp"
+#
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+#
+#   it "doesn't find a non-executable in the root directory"; ( _shpec_failures=0   # shellcheck disable=SC2030
+#     temp=$(make_temp_dir)
+#     validate_dirname "$temp" || return
+#     touch "$temp"/mycommand
+#     find_recursive "$temp" mycommand
+#     assert equal $? 2
+#     assert equal "${command[*]}" mycommand
+#     cleanup "$temp"
+#
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+#
+#   it "finds a command in the first subdirectory"; ( _shpec_failures=0   # shellcheck disable=SC2030
+#     temp=$(make_temp_dir)
+#     validate_dirname "$temp" || return
+#     mkdir "$temp"/mydir
+#     touch "$temp"/mydir/mycommand
+#     chmod 775 "$temp"/mydir/mycommand
+#     find_recursive "$temp" mydir mycommand
+#     assert equal $? 0
+#     # shellcheck disable=SC2154
+#     assert equal "${command[*]}" "$temp"/mydir/mycommand
+#     cleanup "$temp"
+#
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+#
+#   it "finds a command with the same name as the first subdirectory"; ( _shpec_failures=0   # shellcheck disable=SC2030
+#     temp=$(make_temp_dir)
+#     validate_dirname "$temp" || return
+#     mkdir "$temp"/mycommand
+#     touch "$temp"/mycommand/mycommand
+#     chmod 775 "$temp"/mycommand/mycommand
+#     find_recursive "$temp" mycommand
+#     assert equal $? 0
+#     # shellcheck disable=SC2154
+#     assert equal "${command[*]}" "$temp"/mycommand/mycommand
+#     cleanup "$temp"
+#
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+#
+#   it "doesn't find a non-executable file with the same name as the first subdirectory"; ( _shpec_failures=0   # shellcheck disable=SC2030
+#     temp=$(make_temp_dir)
+#     validate_dirname "$temp" || return
+#     mkdir "$temp"/mycommand
+#     touch "$temp"/mycommand/mycommand
+#     find_recursive "$temp" mycommand
+#     assert equal $? 2
+#     assert equal "${command[*]}" mycommand
+#     cleanup "$temp"
+#
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+#
+#   it "doesn't find a directory with the same name as the first subdirectory"; ( _shpec_failures=0   # shellcheck disable=SC2030
+#     temp=$(make_temp_dir)
+#     validate_dirname "$temp" || return
+#     mkdir -p "$temp"/mycommand/mycommand
+#     find_recursive "$temp" mycommand
+#     assert equal $? 1
+#     assert equal "${command[*]}" mycommand
+#     cleanup "$temp"
+#
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+#
+#   it "finds a command in the second-level subdirectory"; ( _shpec_failures=0   # shellcheck disable=SC2030
+#     temp=$(make_temp_dir)
+#     validate_dirname "$temp" || return
+#     mkdir -p "$temp"/mydir/mydir2
+#     touch "$temp"/mydir/mydir2/mycommand
+#     chmod 775 "$temp"/mydir/mydir2/mycommand
+#     find_recursive "$temp" mydir mydir2 mycommand
+#     assert equal $? 0
+#     # shellcheck disable=SC2154
+#     assert equal "${command[*]}" "$temp"/mydir/mydir2/mycommand
+#     cleanup "$temp"
+#
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+#
+#   it "doesn't find a non-existent command in the second-level subdirectory"; ( _shpec_failures=0   # shellcheck disable=SC2030
+#     temp=$(make_temp_dir)
+#     validate_dirname "$temp" || return
+#     mkdir -p "$temp"/mydir/mydir2
+#     find_recursive "$temp" mydir mydir2 mycommand
+#     assert equal $? 1
+#     # shellcheck disable=SC2154
+#     assert equal "${command[*]}" "mydir mydir2 mycommand"
+#     cleanup "$temp"
+#
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+#
+#   it "finds a command in the root directory with arguments"; ( _shpec_failures=0   # shellcheck disable=SC2030
+#     temp=$(make_temp_dir)
+#     validate_dirname "$temp" || return
+#     touch "$temp"/mycommand
+#     chmod 775 "$temp"/mycommand
+#     find_recursive "$temp" mycommand one two
+#     assert equal $? 0
+#     # shellcheck disable=SC2154
+#     assert equal "${command[*]}" "$temp/mycommand one two"
+#     cleanup "$temp"
+#
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+#
+#   it "doesn't find a command which doesn't exist with arguments"; ( _shpec_failures=0   # shellcheck disable=SC2030
+#     temp=$(make_temp_dir)
+#     validate_dirname "$temp" || return
+#     find_recursive "$temp" mycommand one
+#     assert equal $? 1
+#     assert equal "${command[*]}" mycommand
+#     cleanup "$temp"
+#
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+#
+#   it "finds a command in the first subdirectory with arguments"; ( _shpec_failures=0   # shellcheck disable=SC2030
+#     temp=$(make_temp_dir)
+#     validate_dirname "$temp" || return
+#     mkdir "$temp"/mydir
+#     touch "$temp"/mydir/mycommand
+#     chmod 775 "$temp"/mydir/mycommand
+#     find_recursive "$temp" mydir mycommand one two
+#     assert equal $? 0
+#     # shellcheck disable=SC2154
+#     assert equal "${command[*]}" "$temp/mydir/mycommand one two"
+#     cleanup "$temp"
+#
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+#
+#   it "finds a command with the same name as the first subdirectory with arguments"; ( _shpec_failures=0   # shellcheck disable=SC2030
+#     temp=$(make_temp_dir)
+#     validate_dirname "$temp" || return
+#     mkdir "$temp"/mycommand
+#     touch "$temp"/mycommand/mycommand
+#     chmod 775 "$temp"/mycommand/mycommand
+#     find_recursive "$temp" mycommand one two
+#     assert equal $? 0
+#     # shellcheck disable=SC2154
+#     assert equal "${command[*]}" "$temp/mycommand/mycommand one two"
+#     cleanup "$temp"
+#
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
+#
+#   it "finds a command in the second-level subdirectory with arguments"; ( _shpec_failures=0   # shellcheck disable=SC2030
+#     temp=$(make_temp_dir)
+#     validate_dirname "$temp" || return
+#     mkdir -p "$temp"/mydir/mydir2
+#     touch "$temp"/mydir/mydir2/mycommand
+#     chmod 775 "$temp"/mydir/mydir2/mycommand
+#     find_recursive "$temp" mydir mydir2 mycommand one two
+#     assert equal $? 0
+#     # shellcheck disable=SC2154
+#     assert equal "${command[*]}" "$temp/mydir/mydir2/mycommand one two"
+#     cleanup "$temp"
+#
+#     return "$_shpec_failures" ); (( _shpec_failures += $? )) ||:
+#   end
 end
 
 describe 'search_roots'
