51c123026e9667d4114e7858ed4d4aeab2f79237
Ted Lilley <ted.lilley@accelerated.com>
2016-12-27 11:35:02 -0500

    refactor find_command

--- dctk/shpec/dctk_shpec.bash
+++ dctk/shpec/dctk_shpec.bash
@@ -73,12 +73,44 @@ describe 'find_command'
     source "$bin"/dctk
     # shellcheck disable=SC2154
     dir=$($mktempd) || return 1
-    mkdir "$dir"/dir1
-    mkdir "$dir"/dir2
+    # shellcheck disable=SC2154
+    $mkdir "$dir"/dir1
+    $mkdir "$dir"/dir2
     touch "$dir"/dir2/file
     chmod +x "$dir"/dir2/file
     result=$(find_command file "$dir"/dir1 "$dir"/dir2)
     assert equal "$dir"/dir2/file "$result"
+    # shellcheck disable=SC2154
+    cleanup "$dir"
+    return "$_shpec_failures" )
+  end
+
+  # it 'finds the command under bin in a non-libexec root'; (
+  #   source "$bin"/dctk
+  #   # shellcheck disable=SC2154
+  #   dir=$($mktempd) || return 1
+  #   # shellcheck disable=SC2154
+  #   $mkdir "$dir"/dir1
+  #   $mkdir "$dir"/dir2/bin
+  #   touch "$dir"/dir2/bin/file
+  #   chmod +x "$dir"/dir2/bin/file
+  #   result=$(find_command file "$dir"/dir1 "$dir"/dir2)
+  #   assert equal "$dir"/dir2/bin/file "$result"
+  #   # shellcheck disable=SC2154
+  #   cleanup "$dir"
+  #   return "$_shpec_failures" )
+  # end
+end
+
+describe 'search_root'
+  it 'finds a command in libexec'; (
+    source "$bin"/dctk
+    # shellcheck disable=SC2154
+    dir=$($mktempd) || return 1
+    touch "$dir"/file
+    chmod +x "$dir"/file
+    result=$(find_command file dir)
+    assert equal "$dir"/file "$result"
     # shellcheck disable=SC2154
     cleanup "$dir"
     return "$_shpec_failures" )
