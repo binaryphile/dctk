2b2b6db8b19675cb93faa9f791070ee24690fc0c
Ted Lilley <ted.lilley@gmail.com>
2017-11-26 20:21:09 +0000

    update

--- dctk/shpec/dctk_shpec.bash
+++ dctk/shpec/dctk_shpec.bash
@@ -1,269 +1,214 @@
-source shpec-helper.bash
-initialize_shpec_helper
+export TMPDIR=${TMPDIR:-$HOME/tmp}
+mkdir -p "$TMPDIR"
 
-root=$(realpath "$BASH_SOURCE")
-root=$(dirname root)
-root=$(absolute_path "$root"/../..)
-bin=$root/dctk/bin
+set -o nounset
 
-source "$bin"/dctk
+source "$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")"/../bin/dctk
+$($bring '
+  mkdir
+  mktempd
+  rmtree
+  touch
+' from kaizen.commands)
 
-describe 'dctk_exports'
-  it 'sets the _DCTK_ROOT environment variable'; (
-    dctk_exports
-    assert equal "$root"/dctk "$(printenv _DCTK_ROOT)"
-    # shellcheck disable=SC2154
-    return "$_shpec_failures" )
-  end
-end
-
-describe 'find_command'
-  it 'finds a command in libexec'; (
-    # shellcheck disable=SC2154
-    dir=$($mktempd) || return 1
-    touch "$dir"/file
+describe find_command
+  it "finds a command in libexec"; ( _shpec_failures=0
+    dir=$($mktempd) || return
+    $touch "$dir"/file
     chmod +x "$dir"/file
-    result=$(find_command file dir)
-    assert equal "$dir"/file "$result"
-    # shellcheck disable=SC2154
-    cleanup "$dir"
-    return "$_shpec_failures" )
+    find_command file "$dir"
+    assert equal "$dir"/file "$__"
+    $rmtree "$dir"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 
-  it "doesn't find a command not there"; (
-    # shellcheck disable=SC2154
-    dir=$($mktempd) || return 1
-    find_command file dir
+  it "doesn't find a command not there"; ( _shpec_failures=0
+    dir=$($mktempd) || return
+    find_command file "$dir"
     assert unequal 0 $?
-    # shellcheck disable=SC2154
-    cleanup "$dir"
-    return "$_shpec_failures" )
+    $rmtree "$dir"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 
-  it 'finds the first command in a non-libexec root'; (
-    # shellcheck disable=SC2154
-    dir=$($mktempd) || return 1
-    # shellcheck disable=SC2154
+  it "finds the first command in a non-libexec root"; ( _shpec_failures=0
+    dir=$($mktempd) || return
     $mkdir "$dir"/dir1
     $mkdir "$dir"/dir2
-    touch "$dir"/dir2/file
+    $touch "$dir"/dir2/file
     chmod +x "$dir"/dir2/file
-    result=$(find_command file "$dir"/dir1 "$dir"/dir2)
-    assert equal "$dir"/dir2/file "$result"
-    # shellcheck disable=SC2154
-    cleanup "$dir"
-    return "$_shpec_failures" )
+    find_command file "$dir"/dir1 "$dir"/dir2
+    assert equal "$dir"/dir2/file "$__"
+    $rmtree "$dir"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 
-  it 'finds the command under bin in a non-libexec root'; (
-    # shellcheck disable=SC2154
-    dir=$($mktempd) || return 1
-    # shellcheck disable=SC2154
+  it "finds the command under bin in a non-libexec root"; ( _shpec_failures=0
+    dir=$($mktempd) || return
     $mkdir "$dir"/dir1
     $mkdir "$dir"/dir2/bin
-    touch "$dir"/dir2/bin/file
+    $touch "$dir"/dir2/bin/file
     chmod +x "$dir"/dir2/bin/file
-    result=$(find_command file "$dir"/dir1 "$dir"/dir2)
-    assert equal "$dir"/dir2/bin/file "$result"
-    # shellcheck disable=SC2154
-    cleanup "$dir"
-    return "$_shpec_failures" )
+    find_command file "$dir"/dir1 "$dir"/dir2
+    assert equal "$dir"/dir2/bin/file "$__"
+    $rmtree "$dir"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 end
 
-describe 'is_structured'
-  it 'detects a structured directory with bin'; (
-    # shellcheck disable=SC2154
-    dir=$($mktempd) || return 1
-    # shellcheck disable=SC2154
+describe structured?
+  it "detects a structured directory with bin"; ( _shpec_failures=0
+    dir=$($mktempd) || return
     $mkdir "$dir"/bin
-    is_structured dir
+    structured? "$dir"
     assert equal 0 $?
-    # shellcheck disable=SC2154
-    cleanup "$dir"
-    return "$_shpec_failures" )
+    $rmtree "$dir"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 
-  it 'detects a structured directory with libexec'; (
-    # shellcheck disable=SC2154
+  it "detects a structured directory with libexec"; ( _shpec_failures=0
     dir=$($mktempd) || return 1
-    # shellcheck disable=SC2154
     $mkdir "$dir"/libexec
-    is_structured dir
+    structured? "$dir"
     assert equal 0 $?
-    # shellcheck disable=SC2154
-    cleanup "$dir"
-    return "$_shpec_failures" )
+    $rmtree "$dir"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 end
 
-describe 'search_roots'
-  it 'finds a command in a single root dir'; (
-    # shellcheck disable=SC2154
-    dir=$($mktempd) || return 1
+describe search_roots
+  it "finds a command in a single root dir"; ( _shpec_failures=0
+    dir=$($mktempd) || return
     touch "$dir"/file
     chmod +x "$dir"/file
-    result=$(search_roots file [0]="$dir")
-    assert equal "$dir"/file "$result"
-    # shellcheck disable=SC2154
-    cleanup "$dir"
-    return "$_shpec_failures" )
+    search_roots file "$dir"
+    assert equal "$dir"/file "$__"
+    $rmtree "$dir"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 
-  it 'finds a command in a second root dir'; (
-    # shellcheck disable=SC2154
+  it "finds a command in a second root dir"; ( _shpec_failures=0
     dir=$($mktempd) || return 1
     $mkdir "$dir"/dir1
     $mkdir "$dir"/dir2
     touch "$dir"/dir2/file
     chmod +x "$dir"/dir2/file
-    result=$(search_roots file "[0]=$dir/dir1 [1]=$dir/dir2")
-    assert equal "$dir"/dir2/file "$result"
-    # shellcheck disable=SC2154
-    cleanup "$dir"
-    return "$_shpec_failures" )
+    search_roots file "$dir"/dir1 "$dir"/dir2
+    assert equal "$dir"/dir2/file "$__"
+    $rmtree "$dir"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 
-  it "doesn't find a non-existent file"; (
-    # shellcheck disable=SC2154
+  it "doesn't find a non-existent file"; ( _shpec_failures=0
     dir=$($mktempd) || return 1
     $mkdir "$dir"/dir1
     $mkdir "$dir"/dir2
-    search_roots file "[0]=$dir/dir1 [1]=$dir/dir2" >/dev/null
+    search_roots file "$dir"/dir1 "$dir"/dir2
     assert unequal 0 $?
-    # shellcheck disable=SC2154
-    cleanup "$dir"
-    return "$_shpec_failures" )
+    $rmtree "$dir"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 end
 
-describe 'structured_search'
-  it 'finds the command in an unstructured root'; (
-    # shellcheck disable=SC2154
-    dir=$($mktempd) || return 1
-    # shellcheck disable=SC2154
+describe structured_search
+  it "finds the command in an unstructured root"; ( _shpec_failures=0
+    dir=$($mktempd) || return
     $mkdir "$dir"
     touch "$dir"/file
     chmod +x "$dir"/file
-    result=$(structured_search file dir)
-    assert equal "$dir"/file "$result"
-    # shellcheck disable=SC2154
-    cleanup "$dir"
-    return "$_shpec_failures" )
+    structured_search file "$dir"
+    assert equal "$dir"/file "$__"
+    $rmtree "$dir"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 
-  it "doesn't find a non-existent command in an unstructured root"; (
-    # shellcheck disable=SC2154
-    dir=$($mktempd) || return 1
-    # shellcheck disable=SC2154
+  it "doesn't find a non-existent command in an unstructured root"; ( _shpec_failures=0
+    dir=$($mktempd) || return
     $mkdir "$dir"
-    structured_search file dir >/dev/null
+    structured_search file "$dir"
     assert unequal 0 $?
-    # shellcheck disable=SC2154
-    cleanup "$dir"
-    return "$_shpec_failures" )
+    $rmtree "$dir"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 
-  it 'finds the command under bin in a structured root with bin'; (
-    # shellcheck disable=SC2154
-    dir=$($mktempd) || return 1
-    # shellcheck disable=SC2154
+  it "finds the command under bin in a structured root with bin"; ( _shpec_failures=0
+    dir=$($mktempd) || return
     $mkdir "$dir"/bin
-    touch "$dir"/bin/file
+    $touch "$dir"/bin/file
     chmod +x "$dir"/bin/file
-    result=$(structured_search file dir)
-    assert equal "$dir"/bin/file "$result"
-    # shellcheck disable=SC2154
-    cleanup "$dir"
-    return "$_shpec_failures" )
+    structured_search file "$dir"
+    assert equal "$dir"/bin/file "$__"
+    $rmtree "$dir"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 
-  it 'finds the command under bin in a structured root with a root conflict'; (
-    # shellcheck disable=SC2154
-    dir=$($mktempd) || return 1
-    # shellcheck disable=SC2154
-    touch "$dir"/file
+  it "finds the command under bin in a structured root with a root conflict"; ( _shpec_failures=0
+    dir=$($mktempd) || return
+    $touch "$dir"/file
     chmod +x "$dir"/file
     $mkdir "$dir"/bin
-    touch "$dir"/bin/file
+    $touch "$dir"/bin/file
     chmod +x "$dir"/bin/file
-    result=$(structured_search file dir)
-    assert equal "$dir"/bin/file "$result"
-    # shellcheck disable=SC2154
-    cleanup "$dir"
-    return "$_shpec_failures" )
+    structured_search file "$dir"
+    assert equal "$dir"/bin/file "$__"
+    $rmtree "$dir"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 
-  it 'finds the command under libexec in a structured root with libexec'; (
-    # shellcheck disable=SC2154
-    dir=$($mktempd) || return 1
-    # shellcheck disable=SC2154
+  it "finds the command under libexec in a structured root with libexec"; ( _shpec_failures=0
+    dir=$($mktempd) || return
     $mkdir "$dir"/bin
     touch "$dir"/bin/file
     chmod +x "$dir"/bin/file
-    result=$(structured_search file dir)
-    assert equal "$dir"/bin/file "$result"
-    # shellcheck disable=SC2154
-    cleanup "$dir"
-    return "$_shpec_failures" )
+    structured_search file "$dir"
+    assert equal "$dir"/bin/file "$__"
+    $rmtree "$dir"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 
-  it 'finds the command under bin in a structured root with both dirs'; (
-    # shellcheck disable=SC2154
-    dir=$($mktempd) || return 1
-    # shellcheck disable=SC2154
+  it "finds the command under bin in a structured root with both dirs"; ( _shpec_failures=0
+    dir=$($mktempd) || return
     $mkdir "$dir"/bin
     $mkdir "$dir"/libexec
-    touch "$dir"/bin/file
+    $touch "$dir"/bin/file
     chmod +x "$dir"/bin/file
-    result=$(structured_search file dir)
-    assert equal "$dir"/bin/file "$result"
-    # shellcheck disable=SC2154
-    cleanup "$dir"
-    return "$_shpec_failures" )
+    structured_search file "$dir"
+    assert equal "$dir"/bin/file "$__"
+    $rmtree "$dir"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 
-  it 'finds the command under libexec in a structured root with both dirs'; (
-    # shellcheck disable=SC2154
-    dir=$($mktempd) || return 1
-    # shellcheck disable=SC2154
+  it "finds the command under libexec in a structured root with both dirs"; ( _shpec_failures=0
+    dir=$($mktempd) || return
     $mkdir "$dir"/bin
     $mkdir "$dir"/libexec
-    touch "$dir"/libexec/file
+    $touch "$dir"/libexec/file
     chmod +x "$dir"/libexec/file
-    result=$(structured_search file dir)
-    assert equal "$dir"/libexec/file "$result"
-    # shellcheck disable=SC2154
-    cleanup "$dir"
-    return "$_shpec_failures" )
+    structured_search file "$dir"
+    assert equal "$dir"/libexec/file "$__"
+    $rmtree "$dir"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 
-  it 'finds the command under bin in a structured root with both commands'; (
-    # shellcheck disable=SC2154
-    dir=$($mktempd) || return 1
-    # shellcheck disable=SC2154
+  it "finds the command under bin in a structured root with both commands"; ( _shpec_failures=0
+    dir=$($mktempd) || return
     $mkdir "$dir"/bin
     $mkdir "$dir"/libexec
-    touch "$dir"/bin/file
+    $touch "$dir"/bin/file
     chmod +x "$dir"/bin/file
-    touch "$dir"/libexec/file
+    $touch "$dir"/libexec/file
     chmod +x "$dir"/libexec/file
-    result=$(structured_search file dir)
-    assert equal "$dir"/bin/file "$result"
-    # shellcheck disable=SC2154
-    cleanup "$dir"
-    return "$_shpec_failures" )
+    structured_search file "$dir"
+    assert equal "$dir"/bin/file "$__"
+    $rmtree "$dir"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 
-  it "doesn't find a non-existent command in a structured root"; (
-    # shellcheck disable=SC2154
-    dir=$($mktempd) || return 1
-    # shellcheck disable=SC2154
+  it "doesn't find a non-existent command in a structured root"; ( _shpec_failures=0
+    dir=$($mktempd) || return
     $mkdir "$dir"/bin
-    structured_search file dir >/dev/null
+    structured_search file "$dir"
     assert unequal 0 $?
-    # shellcheck disable=SC2154
-    cleanup "$dir"
-    return "$_shpec_failures" )
+    $rmtree "$dir"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 end
