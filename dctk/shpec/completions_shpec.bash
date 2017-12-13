0c3a730b4f37650fc9a6281f9a7b2b5586ce0132
Ted Lilley <ted.lilley@gmail.com>
2017-12-13 04:09:01 +0000

    completions

--- dctk/shpec/completions_shpec.bash
+++ dctk/shpec/completions_shpec.bash
@@ -1,13 +1,14 @@
 set -o nounset
 
-source concorde.bash globals=get
-libexec=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")/../libexec
+source kaizen.bash imports='absolute_dirname get'
+absolute_dirname "$BASH_SOURCE"
+libexec=$__/../libexec
 source "$libexec"/completions
 
 describe completions
   it "outputs a message with input help"; ( _shpec_failures=0
     result=$("$libexec"/completions help)
-    $get <<'    EOS'
+    get <<'    EOS'
       commands
       completions
       help
@@ -17,20 +18,17 @@ describe completions
     return "$_shpec_failures" );: $(( _shpec_failures += $? ))
   end
 
-  # it 'looks for the leading comment'; (
-  #   # shellcheck disable=SC2016
-  #   stub_command grep 'echo "$2"'
-  #   stub_command exec ':'
-  #   result=$(completions_main help)
-  #   # shellcheck disable=SC2154
-  #   assert equal "# completions: true" "$result"
-  #   return "$_shpec_failures" )
-  # end
-  #
-  # it "doesn't return a completion for completions"; (
-  #   result=$("$libexec"/completions completions)
-  #   # shellcheck disable=SC2154
-  #   assert equal "" "$result"
-  #   return "$_shpec_failures" )
-  # end
+  it 'looks for the leading comment'; ( _shpec_failures=0
+    stub_command grep 'command grep -i "${@:2}"'
+    stub_command exec :
+    result=$(main help)
+    assert equal "# Completions: true" "$result"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
+  end
+
+  it "doesn't return a completion for completions"; ( _shpec_failures=0
+    result=$("$libexec"/completions completions)
+    assert equal '' "$result"
+    return "$_shpec_failures" );: $(( _shpec_failures += $? ))
+  end
 end
